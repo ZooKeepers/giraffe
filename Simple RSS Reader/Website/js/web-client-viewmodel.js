cbf1a33de3cb5a11b6979c1bd771d15044c52589
chad-stewart <chad.stewart@gatech.edu>
2013-09-02 19:41:33 -0400

    Updated simple web page

---
+++ Simple RSS Reader/Website/js/web-client-viewmodel.js
@@ -0,0 +1,30 @@
+function RSSFeed(url, text, id) {
+	this.url = url;
+	this.text = url;
+	this.id = id;
+}
+
+function SimpleWebClientViewModel() {
+    var vm = this;
+	
+	vm.feeds = ko.observableArray([
+		new RSSFeed("http://www.google.com/rss",
+			"First Update",
+			1),
+		new RSSFeed("http://www.microsoft.com/rss",
+			"Second Update",
+			2),
+		new RSSFeed("http://www.amazon.com/rss",
+			"Third Update",
+			3)
+		]);
+}
+
+(function ($, window, document) {
+    $(document).ready(function () {
+
+        window.swcvm = new SimpleWebClientViewModel();
+        ko.applyBindings(swcvm);
+		
+    });
+}($, window, document));
