bc19a207bed1ef3c42a42474cb5ce5c51220cdc9
chad-stewart <chad.stewart@gatech.edu>
2013-09-19 20:38:22 -0400

    [Finishes #56316538] Shows list of clickable feeds
    Users can click on their list of feeds and view the specified items.

--- client/js/web-client-viewmodel.js
+++ client/js/web-client-viewmodel.js
@@ -8,52 +8,93 @@ function SimpleWebClientViewModel() {
     var vm = this;
 	var urlBase = 'http://localhost:3000/';
 	
-	vm.feeds = ko.observableArray([
-		new RSSFeed("http://www.google.com/rss",
-			"First Update",
-			1),
-		new RSSFeed("http://www.microsoft.com/rss",
-			"Second Update",
-			2),
-		new RSSFeed("http://www.amazon.com/rss",
-			"Third Update",
-			3)
-		]);
-	
+	vm.currentFeed = ko.observable('All Feeds');
 	vm.user = ko.observable({});
-	
+	vm.feeds = ko.observableArray([]);
+
 	function getUserInfo() {
 		$.ajax({
 		    url: urlBase + 'user',
 			dataType: "json",
 			success: function (data) {
 				vm.user({
-					username: data.username,
+					username: toTitleCase(data.username),
 					feeds: data.feeds,
 					id: data._id
 				});
+				
+				getFeedData();
 			}
 		});
 	}
 	
-	vm.feedItems = ko.observableArray([]);
+	function toTitleCase(str)
+	{
+		return str.replace(/\w\S*/g, function(txt){
+			return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+		});
+	}
 	
-	function getTestRSSFeed() {
-		$.ajax({
-		    url: urlBase + 'rss',
-			dataType: "json",
-			success: function (data) {
-				ko.utils.arrayForEach(data.rss.channel[0].item, function(item) {
-					vm.feedItems.push({
-						feed: data.rss.channel[0].title[0],
-						description: item.description[0],
-						title: item.title[0],
-						author: item['dc:creator'][0]
+	vm.displayedItems = ko.observableArray([]);
+	
+	// Changes the display to a specific feed
+	// bug: requeries atm, maybe cache instead?
+	vm.changeFeed = function (feed) {
+		vm.currentFeed(feed.title);
+		
+		vm.displayedItems([]);
+		getFeedItems(feed);
+	};
+	
+	// Get items for all feeds!
+	// big: requeries atm, maybe cache instead?
+	vm.showAllFeeds = function () {
+		vm.currentFeed('All Feeds');
+		
+		vm.displayedItems([]);
+		
+		ko.utils.arrayForEach(vm.feeds(), function(feed) {
+			getFeedItems(feed);
+		});
+	};
+	
+	// Get information about the current feeds
+	// -- Also pulls the items of that feed
+	function getFeedData() {
+		ko.utils.arrayForEach(vm.user().feeds, function(feed) {
+			$.ajax({
+				url: urlBase + 'feed/' + encodeURIComponent(feed.url),
+				dataType: "json",
+				success: function (data) {
+					vm.feeds.push({
+						feed: data[0].feed,
+						description: data[0].description,
+						title: data[0].title,
+						id: data[0]._id
 					});
 					
-					vm.y = 0;
+					getFeedItems(vm.feeds()[vm.feeds().length - 1]);
+				}
+			});
+		});
+	}
+	
+	// Pull the items from a specific feed
+	function getFeedItems(feed) {
+		$.ajax({
+		    url: urlBase + 'articles/' + encodeURIComponent(feed.feed),
+			dataType: "json",
+			success: function (data) {
+				ko.utils.arrayForEach(data, function(item) {
+					vm.displayedItems.push({
+						feed: feed.title,
+						description: item.description,
+						title: item.title,
+						timestamp: item.pubDate,
+						author: item.author,
+						id: item._id
+					});
 				});
-				
 			}
 		});
 	}
@@ -63,21 +104,12 @@ function SimpleWebClientViewModel() {
         return "#" + str + comp.replace(/\W/g, '');
     };
 	
+	// Utility functions for generating the href links for the Accordion Elements
     vm.genAccId = function (str, comp) {
         return "" + str + comp.replace(/\W/g, '');
     };
 	
 	getUserInfo();
-	
-	getTestRSSFeed();
-	/* Reading a RSS feed
-	jQuery.getFeed({
-		url: 'rss.xml',
-		success: function(feed) {
-			alert(feed.title);
-		}
-	});
-	*/
 }
 
 (function ($, window, document) {
