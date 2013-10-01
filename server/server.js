a6b91a5297a0491f0f658c2c95dbee89091c6857
bkvchu <bkvchu@gmail.com>
2013-10-01 13:43:11 -0400

    update server

--- server/server.js
+++ server/server.js
@@ -208,4 +208,4 @@ var port = process.env.PORT || 5000;
 app.listen(port, function(){
 console.log("Listening on "+port);});
 
-console.log('Listening on port 5000');
+//console.log('Listening on port 5000');
