4bb1e7454cda99f2fef008dd15cbabc2d299bd87
bkvchu <bkvchu@gmail.com>
2013-10-01 23:03:54 -0400

    Added heroku functionality
    Moved the package.json and created Procfile that will be used for
    pushing the application to heroku.
    server.js was edited so that it will use the process port if provide,
    or else it will use the old default port 3000

--- server/server.js
+++ server/server.js
@@ -204,5 +204,6 @@ var populateDB = function() {
     });*/
 };
 
-app.listen(3000);
-console.log('Listening on port 3000');
+var port = process.env.PORT || 3000;
+app.listen(port, function(){
+console.log("Listening on "+port);});
