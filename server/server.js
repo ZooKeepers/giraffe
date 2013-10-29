8ddd089727beb591b1676dae8d70a45d4ac04dfa
bkvchu <bkvchu@gmail.com>
2013-10-29 15:28:58 -0400

    ievert "issues with closing server"
    This reverts commit 152b73a8bf869468367bf4f15d7f45443d31056d.
    
    reverting to working state

--- server/server.js
+++ server/server.js
@@ -77,8 +77,6 @@ passport.use(new LocalStrategy(function(username, password, done) {
     console.log("SOMEONE'S LOGGING IN")
     db.collection('users', function(err, collection) {
         console.log("collection: "+collection);
-        console.log("username: " + username);
-        console.log("");
         collection.findOne({'username': username}, function(err, item) {
             if (err) {
                 console.log("Authentication error: "+err);
@@ -364,6 +362,7 @@ var populateDB = function() {
         collection.insert(articles, {safe:true}, function(err, result) {});
     });*/
 };
+db.close();
 var port = process.env.PORT || 3000;
 app.listen(port, function(){
 console.log("Listening on "+port);});
